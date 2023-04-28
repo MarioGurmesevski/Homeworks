9f6f439c3b5f16d6ac9edbca9cd9d81dd06765a2
MarioGurmesevski <mariogurmesevski1@gmail.com>
2023-04-28 23:16:24 +0200

    c

---
+++ Advanced_Node_Homeworks/Node_Zoo_homework/V5/v5/src/animals/dtos/animal-query.dto.ts
@@ -0,0 +1,42 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Validate,
+} from 'class-validator';
+
+function isAgeValid(age: number): boolean {
+  return age >= 5;
+}
+
+enum Gender {
+  M = 'Male',
+  F = 'Female',
+}
+
+export class animalQueryDto {
+  @IsString()
+  @IsOptional()
+  @ApiPropertyOptional({
+    type: String,
+    example: 'Skopje',
+  })
+  location?: string;
+
+  @IsNumber()
+  @IsOptional()
+  @Validate(isAgeValid)
+  @ApiPropertyOptional({
+    type: Number,
+    example: 6,
+    default: 6,
+  })
+  age?: number;
+
+  @IsEnum(Gender)
+  @IsOptional()
+  @ApiPropertyOptional({})
+  gender: Gender;
+}
